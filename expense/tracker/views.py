98e0c8ca00a74cc4369cac9e5937dd0815886817
Piotr <piotr.wirth@onet.pl>
2023-03-01 18:10:24 +0100

    Modified post request for expenses , added expenses display

--- expense/tracker/views.py
+++ expense/tracker/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .forms import ExpenseForm
+from .models import Expense
+from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
     if request.method =='POST':
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
+            return HttpResponseRedirect('/')
+    
+    expenses = Expense.objects.all()
     expense_form = ExpenseForm()
-    return render(request,'tracker/index.html', {'expense_form':expense_form})
+    return render(request,'tracker/index.html', {'expense_form':expense_form,'expenses':expenses})
