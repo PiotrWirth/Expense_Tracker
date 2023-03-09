45d59fc1da2e24688d8af94be390392a3817f20d
Piotr <piotr.wirth@onet.pl>
2023-03-09 21:16:52 +0100

    Added logic for weekly, monthly and yearly expenses

--- expense/tracker/views.py
+++ expense/tracker/views.py
@@ -3,6 +3,7 @@ from .forms import ExpenseForm
 from .models import Expense
 from django.http import HttpResponseRedirect
 from django.db.models import Sum
+import datetime
 # Create your views here.
 def index(request):
     if request.method =='POST':
@@ -13,8 +14,22 @@ def index(request):
     
     expenses = Expense.objects.all()
     total_expenses = expenses.aggregate(Sum('amount'))
+
+
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__qt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
+
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__qt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__qt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+
     expense_form = ExpenseForm()
-    return render(request,'tracker/index.html', {'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses})
+    return render(request,'tracker/index.html', {'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses,'yearly_sum':yearly_sum,'monthly_sum':monthly_sum,'weekly_sum':weekly_sum})
 
 def edit(request,id):
     expense = Expense.objects.get(id=id)
