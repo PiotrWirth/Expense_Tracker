e757838f1cbcd4910a7b156ed15488be1f756ed5
Piotr <piotr.wirth@onet.pl>
2023-03-11 17:38:17 +0100

    Added and styled daily expenses functionality

--- expense/tracker/views.py
+++ expense/tracker/views.py
@@ -28,8 +28,10 @@ def index(request):
     data = Expense.objects.filter(date__gt=last_week)
     weekly_sum = data.aggregate(Sum('amount'))
 
+    daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+
     expense_form = ExpenseForm()
-    return render(request,'tracker/index.html', {'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses,'yearly_sum':yearly_sum,'monthly_sum':monthly_sum,'weekly_sum':weekly_sum})
+    return render(request,'tracker/index.html', {'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses,'yearly_sum':yearly_sum,'monthly_sum':monthly_sum,'weekly_sum':weekly_sum,'daily_sums':daily_sums})
 
 def edit(request,id):
     expense = Expense.objects.get(id=id)
